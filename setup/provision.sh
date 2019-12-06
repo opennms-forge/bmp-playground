d76f9b0f9a1f52b8887310aa4d18bc04c024c06e
Ronny Trommer <ronny@opennms.org>
2019-12-06 14:39:25 -0500

    Initial commit

---
+++ setup/provision.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+OPENNMS_HOST="localhost"
+OPENNMS_USER="admin"
+OPENNMS_PASS="admin"
+OPENNMS_PORT="8980"
+
+echo -n "Ensure the ReST API is running before setup        "
+until $(curl -L --output /dev/null --silent --head --fail http://${OPENNMS_HOST}:8980); do
+    printf '.'
+    sleep 2
+done
+echo "    DONE"
+
+#
+# Setup Demo data with foreign sources, requisitions and a topology
+#
+echo -n "Create Foreign Sources                             ... "
+curl -s -u ${OPENNMS_USER}:${OPENNMS_PASS} \
+     -X POST \
+     -H "Content-Type: application/xml" \
+     -H "Accept: application/xml" \
+     -d @foreign-source.xml \
+     http://${OPENNMS_HOST}:${OPENNMS_PORT}/opennms/rest/foreignSources
+echo "DONE"
+
+echo -n "Create Requisition OpenNMS Stack                   ... "
+curl -s -u ${OPENNMS_USER}:${OPENNMS_PASS} \
+     -X POST \
+     -H "Content-Type: application/xml" \
+     -H "Accept: application/xml" \
+     -d @opennms-stack.xml \
+     http://${OPENNMS_HOST}:${OPENNMS_PORT}/opennms/rest/requisitions
+echo "DONE"
+
+echo -n "Create Requisition BGP Router                      ... "
+curl -s -u ${OPENNMS_USER}:${OPENNMS_PASS} \
+     -X POST \
+     -H "Content-Type: application/xml" \
+     -H "Accept: application/xml" \
+     -d @bgp-routers.xml \
+     http://${OPENNMS_HOST}:${OPENNMS_PORT}/opennms/rest/requisitions
+echo "DONE"
+
+echo -n "Import requisition for OpenNMS Stack               ... "
+curl -s -u ${OPENNMS_USER}:${OPENNMS_PASS} \
+     -X PUT \
+     http://${OPENNMS_HOST}:${OPENNMS_PORT}/opennms/rest/requisitions/opennms-stack/import
+echo "DONE"
+
+echo -n "Import requisition for BGP Router                  ... "
+curl -s -u ${OPENNMS_USER}:${OPENNMS_PASS} \
+     -X PUT \
+     http://${OPENNMS_HOST}:${OPENNMS_PORT}/opennms/rest/requisitions/bgp-routers/import
+echo "DONE"
+
+echo -n "Create GraphML layer 3 network                     ... "
+curl -s -u ${OPENNMS_USER}:${OPENNMS_PASS} \
+    -X DELETE \
+    http://${OPENNMS_HOST}:${OPENNMS_PORT}/opennms/rest/graphml/network-layer3
+sleep 2
+curl -s -u ${OPENNMS_USER}:${OPENNMS_PASS} \
+    -X POST \
+    -H "Content-Type: application/xml" \
+    -d @network-layer3.xml \
+    http://${OPENNMS_HOST}:${OPENNMS_PORT}/opennms/rest/graphml/network-layer3
+echo "DONE"
+
+
+echo -n "Create GraphML BGP network                         ... "
+curl -s -u ${OPENNMS_USER}:${OPENNMS_PASS} \
+    -X DELETE \
+    http://${OPENNMS_HOST}:${OPENNMS_PORT}/opennms/rest/graphml/network-bgp
+sleep 2
+curl -s -u ${OPENNMS_USER}:${OPENNMS_PASS} \
+    -X POST \
+    -H "Content-Type: application/xml" \
+    -d @network-bgp.xml \
+    http://${OPENNMS_HOST}:${OPENNMS_PORT}/opennms/rest/graphml/network-bgp
+echo "DONE"
